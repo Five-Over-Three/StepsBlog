91f732a66bda5a543512b073e1b23f9535acc57a
FiveOverThree <fiveoverthreetv@gmail.com>
2025-09-22 12:47:49 +0100

    Added blog homepage + link to blog in headers

--- app.py
+++ app.py
@@ -71,5 +71,5 @@ def uploader():
     else:
         return render_template('input.html', form=form)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
