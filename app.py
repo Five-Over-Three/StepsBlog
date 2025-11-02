2ac1fd282c38faf3a06f9e376d31c7230118e4dd
FiveOverThree <fiveoverthreetv@gmail.com>
2025-11-02 12:28:22 +0000

    Blog page now dynamically generates a calendar

--- app.py
+++ app.py
@@ -23,11 +23,13 @@ form = UploadForm()
 #set up to write data
 col_names = [['year', 'month', 'day', 'steps', 'distance', 'pushups', 'situps', 'squats', 'weight', 'comment']]
 date_format = '%Y-%m-%d'
+myCal = calendar.Calendar()
 
 @app.route('/')
 def blog():
     #TODO - need homepage stuffs here e.g. link to latest post, calendar, graphs etc.
-    return render_template('index.html')
+    myMonth = myCal.monthdayscalendar(2025, 11)
+    return render_template('index.html', myMonth=myMonth)
 
 @app.route('/<int:year>/<int:month>/<int:day>')
 def show_day(year, month, day):
